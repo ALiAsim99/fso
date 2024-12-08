1b9cf36bd8d8fb0214d421cddf0d1672fdba61b4
ali.99asim <ali.99asim@gmail.com>
2024-12-08 14:34:30 +0500

    completed backend

---
+++ Part2/phonebook/BACKEND/index.js
@@ -0,0 +1,101 @@
+const express=require("express");
+const cors = require('cors')
+
+const app=express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use(express.static('dist'))
+
+let persons=[
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/api/persons',(req,res)=>{
+    res.json(persons)
+})
+
+app.get("/info",(req,res)=>{
+    let date=new Date();
+    let text=`<p>Phonebook has info for ${persons.length} people<p><br/>${date}<p>`;
+    res.send(text);
+})
+
+app.get("/api/persons/:id",(req,res)=>{
+   const id=req.params.id;
+   const person=persons.find(p=>p.id==id);
+   if(person){
+    res.send(person)
+   }else{
+    res.status(404).end("Not found ---- xxxx");
+   }
+})
+app.delete("/api/persons/:id",(req,res)=>{
+    const id=req.params.id;
+    const person=persons.find(p=>p.id==id);
+    if(person){
+        console.log(123);
+        persons=persons.filter(p=>p.id!=id);
+        res.status(204).end("Deleted")
+    }else{
+        res.status(404).end("No id found");
+    }
+})
+
+const generateId=()=>{
+    const id=persons.length>0?Math.max(...persons.map(p=>Number(p.id))):0;
+    return String(id+1);
+}
+
+app.post("/api/persons",(req,res)=>{
+    const body=req.body;
+    
+    if(!body.name || !body.number){
+        res.status(400).json({
+            error:"Missing"
+        });
+        return;
+    }
+    const duplicatePerson=persons.find(p=>p.name==body.name);
+    console.log(duplicatePerson)
+    if(duplicatePerson){
+        res.status(200).json({
+            error:'name must be unique'
+        })
+        return;
+    }
+    const person={
+        name:body.name,
+        number:body.number,
+        id:generateId()
+    }
+  
+    persons=persons.concat(person);
+    res.json(body);
+    
+})
+
+const PORT = process.env.PORT || 3001
+
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
---
+++ Part2/phonebook/Frontend/src/App.jsx
@@ -0,0 +1,94 @@
+import { useEffect, useState } from 'react'
+import Filter from './components/Filter'
+import Persons from './components/Persons'
+import Form from './components/Form'
+import Notification from './components/Notification'
+import personService from './service/person'
+
+const App = () => {
+  const [persons, setPersons] = useState([]) 
+  const [newName, setNewName] = useState('')
+  const [newNumber,setNewNumber]=useState('')
+  const [personFilter,setFilter]=useState('')
+  const [message,setMessage]=useState(null);
+
+  useEffect(()=>{
+    personService.getAll()
+    .then(initialPerson=>{
+      setPersons(initialPerson)});
+    
+  },[])
+
+  const deletePerson=(id)=>{
+    const findPerson=persons.find(p=>p.id==id);
+    const confirm=window.confirm(`Delete ${findPerson.name}`);
+    if(confirm){
+      personService
+      .deletePerson(id)
+      .then(returnPerson=>{
+        setPersons(persons.filter(p=>p.id!==id))
+        setMessage(`${findPerson.name} has been deleted successfully`);
+        setTimeout(()=>setMessage(null),500);
+      });
+    }
+
+  }
+
+  const addPerson=(e)=>{
+    e.preventDefault();
+    const duplicatePerson=persons.find(p=>p.name==newName);
+    
+    if(duplicatePerson){
+      let confirm=window.confirm(`${newName} already exists.Do you want to update the phone number?`);
+      if(confirm){
+          const id=duplicatePerson.id;
+          const newPerson={...duplicatePerson,number:newNumber};
+          personService
+          .update(id,newPerson)
+          .then(returnPerson=>setPersons(persons.map(p=>p.id==id?returnPerson:p)))
+          .catch(e=>{
+            setMessage(`${duplicatePerson.name} has already been removed`);
+            setTimeout(()=>setMessage(null),500);
+            setPersons(persons.filter(p=>p.id!==id));
+          })
+          setNewName("")
+          setNewNumber("")
+      }
+      return;
+      
+    }
+    const newPerson={
+      name:newName,
+      number:newNumber
+    }
+    
+    personService.create(newPerson)
+    .then(person=> {
+      setPersons(persons.concat(person))
+      setMessage(`${newPerson.name} has been added successfully`);
+        setTimeout(()=>setMessage(null),500);
+    })
+    setNewName("")
+    setNewNumber("")
+  }
+
+  const handleNameChange=e=>{
+    setNewName(e.target.value)
+  }
+  const handleNumberChange=e=>{
+    setNewNumber(e.target.value)
+  }
+  const showPersons=personFilter?persons.filter(p=>p.name.toLowerCase().includes(personFilter.toLowerCase())):persons;
+
+  return (
+    <div>
+      <Notification message={message}/>
+      <Filter value={personFilter} setFilter={setFilter}/>
+      <Form addPerson={addPerson} newName={newName} newNumber={newNumber} handleNameChange={handleNameChange} handleNumberChange={handleNumberChange}/>
+      <Persons showPersons={showPersons} deletePerson={deletePerson}/>
+    </div>
+  )
+}
+
+export default App
+
